d6a052877083c03fc34964118394b03675cc0fdd
correafe <fecorrea95@gmail.com>
2025-08-07 17:32:20 -0300

    segundo commit

--- basico.py
+++ basico.py
@@ -1,3 +1,5 @@
+#Arquivo para aprendizado da parte básica de python
+
 
 #comentários e print
 """docstrings não são comentários"""
@@ -285,34 +287,25 @@ Se nada for digitado em nome ou idade:
 #     numero_1 = input('Digite um número: ')
 #     numero_2 = input('Digite outro número: ')
 #     operador = input('Digite o operador (+-/*): ')
-
 #     numeros_validos = None
-
 #     try:
 #         num_1_float = float(numero_1)
 #         num_2_float = float(numero_2)
 #         numeros_validos = True
 #     except:
 #         numeros_validos = None
-
 #     if numeros_validos is None:
 #         print('Um ou ambos os números digitados são inválidos.')
 #         continue
-
 #     operadores_permitidos = '+-/*'
-
 #     if operador not in operadores_permitidos:
 #         print('Operador inválido.')
 #         continue
-
 #     if len(operador) > 1:
 #         print('Digite apenas um operador.')
 #         continue
-
 #     ###
-
 #     sair = input('Quer sair? [s]im: ').lower().startswith('s')
-
 #     if sair is True:
 #         break
 
