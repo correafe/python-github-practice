815bcd986258d513e4a81cc6f90cff154ff67905
correafe <fecorrea95@gmail.com>
2025-08-20 22:59:00 -0300

    metade

--- intermediario.py
+++ intermediario.py
@@ -309,4 +309,239 @@
 # print('Você acertou', qtd_acertos)
 # print('de', len(perguntas), 'perguntas.')
 
+#set 
+# s1 = set()
+# s1 = {'felipe', 1, 2, 3}
+# s2 = set('felipe')
+# print(s1, s2)
+# print(s2)
+# print(1 in s1)
+# s1.add(7)
+# s1.update(('Olá mundo', 1, 2, 3, 4))
+# # s1.clear()
+# s1.discard('Olá mundo')
+# s1.discard('Luiz')
+# print(s1)
+# Operadores úteis:
+# união | união (union) - Une
+# intersecção & (intersection) - Itens presentes em ambos
+# diferença - Itens presentes apenas no set da esquerda
+# diferença simétrica ^ - Itens que não estão em ambos
+# s1 = {1, 2, 3}
+# s2 = {2, 3, 4}
+# s3 = s1 | s2
+# s3 = s1 & s2
+# s3 = s2 - s1
+# s3 = s1 ^ s2
+# print(s3)
 
+# Exemplo de uso dos sets
+# letras = set()
+# while True:
+#     letra = input('Digite: ')
+#     letras.add(letra.lower())
+#     if 'l' in letras:
+#         print('PARABÉNS')
+#         break
+#     print(letras)
+
+"""
+Exercício
+Crie uma função que encontra o primeiro duplicado considerando o segundo
+número como a duplicação. Retorne a duplicação considerada.
+Requisitos:
+    A ordem do número duplicado é considerada a partir da segunda
+    ocorrência do número, ou seja, o número duplicado em si.
+    Exemplo:
+        [1, 2, 3, ->3<-, 2, 1] -> 1, 2 e 3 são duplicados (retorne 3)
+        [1, 2, 3, 4, 5, 6] -> Retorne -1 (não tem duplicados)
+        [1, 4, 9, 8, ->9<-, 4, 8] (retorne 9)
+    Se não encontrar duplicados na lista, retorne -1
+"""
+# lista_de_listas_de_inteiros = [
+#     [1, 1, 3, 4, 5, 6, 7, 8, 9, 10],
+#     [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+#     [9, 1, 8, 9, 9, 7, 2, 1, 6, 8],
+#     [1, 3, 2, 2, 8, 6, 5, 9, 6, 7],
+#     [3, 8, 2, 8, 6, 7, 7, 3, 1, 9],
+#     [4, 8, 8, 8, 5, 1, 10, 3, 1, 7],
+#     [1, 3, 7, 2, 2, 1, 5, 1, 9, 9],
+#     [10, 2, 2, 1, 3, 5, 10, 5, 10, 1],
+#     [1, 6, 1, 5, 1, 1, 1, 4, 7, 3],
+#     [1, 3, 7, 1, 10, 5, 9, 2, 5, 7],
+#     [4, 7, 6, 5, 2, 9, 2, 1, 2, 1],
+#     [5, 3, 1, 8, 5, 7, 1, 8, 8, 7],
+#     [10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
+# ]
+
+#o que eu consegui fazer:
+# def duplicados ():
+#     contadorlista = 0
+#     contadoritem = 0
+#     for lista in lista_de_listas_de_inteiros:
+#         contadorlista += 1
+
+#         for item in lista:
+#             print(contadoritem, item)
+#             contadoritem += 1
+#         contadoritem = 0
+
+#o que era:
+# def encontra_primeiro_duplicado(lista_de_inteiros):
+#     numeros_checados = set()
+#     primeiro_duplicado = -1
+
+#     for numero in lista_de_inteiros:
+#         if numero in numeros_checados:
+#             primeiro_duplicado = numero
+#             break
+
+#         numeros_checados.add(numero)
+
+#     return primeiro_duplicado
+
+
+# for lista in lista_de_listas_de_inteiros:
+#     print(
+#         lista,
+#         encontra_primeiro_duplicado(lista)
+#     )
+
+#função lambda
+# lista = [4, 32, 1, 34, 5, 6, 6, 21, ]
+# lista.sort(reverse=True)
+# sorted(lista)
+# lista = [
+#     {'nome': 'Luiz', 'sobrenome': 'miranda'},
+#     {'nome': 'Maria', 'sobrenome': 'Oliveira'},
+#     {'nome': 'Daniel', 'sobrenome': 'Silva'},
+#     {'nome': 'Eduardo', 'sobrenome': 'Moreira'},
+#     {'nome': 'Aline', 'sobrenome': 'Souza'},
+# ]
+# def exibir(lista):
+#     for item in lista:
+#         print(item)
+#     print()
+# l1 = sorted(lista, key=lambda item: item['nome'])
+# l2 = sorted(lista, key=lambda item: item['sobrenome'])
+# exibir(l1)
+# exibir(l2)
+
+#lambda complexo
+# def executa(funcao, *args):
+#     return funcao(*args)
+# def soma(x, y):
+#     return x + y
+# def cria_multiplicador(multiplicador):
+#     def multiplica(numero):
+#         return numero * multiplicador
+#     return multiplica
+# duplica = cria_multiplicador(2)
+# duplica = executa(
+#     lambda m: lambda n: n * m,
+#     2
+# )
+# print(duplica(2))
+# print(
+#     executa(
+#         lambda x, y: x + y,
+#         2, 3
+#     ),
+# )
+# print(
+#     executa(
+#         lambda *args: sum(args),
+#         1, 2, 3, 4, 5, 6, 7
+#     )
+# )
+
+#*args e *kwargs
+# Empacotamento e desempacotamento de dicionários
+# a, b = 1, 2
+# a, b = b, a
+# print(a, b)
+# (a1, a2), (b1, b2) = pessoa.items()
+# print(a1, a2)
+# print(b1, b2)
+# for chave, valor in pessoa.items():
+#     print(chave, valor)
+# pessoa = {
+#     'nome': 'Aline',
+#     'sobrenome': 'Souza',
+# }
+# dados_pessoa = {
+#     'idade': 16,
+#     'altura': 1.6,
+# }
+# pessoas_completa = {**pessoa, **dados_pessoa}
+# print(pessoas_completa)
+# args e kwargs
+# args (já vimos)
+# kwargs - keyword arguments (argumentos nomeados)
+# def mostro_argumentos_nomeados(*args, **kwargs):
+#     print('NÃO NOMEADOS:', args)
+
+#     for chave, valor in kwargs.items():
+#         print(chave, valor)
+# mostro_argumentos_nomeados(nome='Joana', qlq=123)
+# mostro_argumentos_nomeados(**pessoas_completa)
+# configuracoes = {
+#     'arg1': 1,
+#     'arg2': 2,
+#     'arg3': 3,
+#     'arg4': 4,
+# }
+# mostro_argumentos_nomeados(**configuracoes)
+
+#list comprehension
+# lista = []
+# for numero in range(10):
+#     lista.append(numero)
+# # print(lista)
+# lista = [
+#     numero * 2
+#     for numero in range(10)
+# ]
+# print(lista)
+
+# mapeamento de dados em list comprehension
+# produtos = [
+#     {'nome': 'p1', 'preco': 20, },
+#     {'nome': 'p2', 'preco': 10, },
+#     {'nome': 'p3', 'preco': 30, },
+# ]
+# novos_produtos = [
+#     {**produto, 'preco': produto['preco'] * 1.05}
+#     if produto['preco'] > 20 else {**produto}
+#     for produto in produtos
+# ]
+# # print(novos_produtos)
+# print(*novos_produtos, sep='\n')
+
+# import pprint
+# def p(v):
+#     pprint.pprint(v, sort_dicts=False, width=40)
+# p(novos_produtos)
+# lista = [n for n in range(10) if n < 5]
+# novos_produtos = [
+#     {**produto, 'preco': produto['preco'] * 1.05}
+#     if produto['preco'] > 20 else {**produto}
+#     for produto in produtos
+#     if (produto['preco'] >= 20 and produto['preco'] * 1.05) > 10
+# ]
+# p(novos_produtos)
+
+# lista = []
+# for x in range(3):
+#     for y in range(3):
+#         lista.append((x, y))
+# lista = [
+#     (x, y)
+#     for x in range(3)
+#     for y in range(3)
+# ]
+# lista = [
+#     [(x, letra) for letra in 'Luiz']
+#     for x in range(3)
+# ]
+# print(lista)
